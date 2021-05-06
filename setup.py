44e51549456e9aea95f5ccfcd2b6948bdb4b3072
Daniel Uhříček <daniel.uhricek@gypri.cz>
2021-05-06 20:30:25 +0200

    Release setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as f:
     long_description = f.read()
 
-with open("requirements.txt", "r") as f:
-    install_requires = f.read().splitlines()
+
+install_requires = ["numpy", "pandas", "scikit-learn", "matplotlib", "seaborn"]
 
 setup(
-    name="fet",
-    version="0.1.0",
+    name="nemea-fet",
+    version="0.1.1",
     author="Daniel Uhříček",
     author_email="daniel.uhricek@gypri.cz",
-    url="https://github.com/danieluhricek/fet",
-    description="Feature Exploration Toolkit",
+    url="https://github.com/danieluhricek/nemea-fet",
+    description="Nemea Feature Exploration Toolkit",
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=install_requires,
